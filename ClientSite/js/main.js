e7dacd13327705faa84d4a5ec8ff242a395340e9
kylep923 <8kylepetersburg8@gmail.com>
2017-12-05 13:11:23 -0600

    Functionality added to ClientSite: Add to database. Delete from database. Check if grid exists before adding.

--- ClientSite/js/main.js
+++ ClientSite/js/main.js
@@ -25,6 +25,8 @@ var grid_size_y = 36;
 var storeArray = [];
 var enterStoreMode = false;
 var scaleObj;
+var grid_name = "";
+var grid_description = "";
 
 
 
@@ -38,54 +40,66 @@ submit.onclick = function() {
 
     //drawArea.style.backgroundImage = 'url("' + input.value + '")';
 
-    var image = document.createElement("IMG");
-    image.setAttribute("src", input.value);
-    image.setAttribute("id", "drawImage");
-    image.setAttribute("style", "visibility: hidden;");
-    document.body.appendChild(image);
-    var drawImage = document.getElementById("drawImage");
-    var imageHeight = image.clientHeight;
-    var imageWidth = image.clientWidth;
+    var image = document.getElementById("drawImage");
+    var downloadingImage = new Image();
+    downloadingImage.onload = function() {
+        image.src = this.src;
 
-    //drawArea.setAttribute("style", "width:" + imageWidth + "px; height:" + imageHeight + "px; background-image: " + 'url("' + input.value + '");');
+        //image.setAttribute("src", input.value);
+        //while (!image.complete) { console.log(image.complete) }
+        //image.setAttribute("id", "drawImage");
+        image.setAttribute("style", "visibility: hidden;");
+        if (image.complete) {
+            document.body.appendChild(image);
+            var drawImage = document.getElementById("drawImage");
+            var imageHeight = image.clientHeight;
+            var imageWidth = image.clientWidth;
+        }
 
-    console.log(item);
-    //console.log(scaleArray);
+        //drawArea.setAttribute("style", "width:" + imageWidth + "px; height:" + imageHeight + "px; background-image: " + 'url("' + input.value + '");');
 
-    $("#drawImage").remove();
+        //console.log(item);
+        //console.log(scaleArray);
 
-    $("rect").remove();
-    View.startNode = false;
-    View.endNode = false;
-    View.nodeSize = tile_Size.value;
+        //$("#drawImage").remove();
+        $("rect").remove();
+        View.startNode = false;
+        View.endNode = false;
+        View.nodeSize = tile_Size.value;
 
-    Controller.gridSize[0] = Math.round(imageWidth / parseInt(tile_Size.value));
-    Controller.gridSize[1] = Math.round(imageHeight / parseInt(tile_Size.value));
+        console.log(Math.round(imageWidth / parseInt(tile_Size.value)));
+        console.log(Math.round(imageHeight / parseInt(tile_Size.value)));
 
-    var numCols = Controller.gridSize[0],
-        numRows = Controller.gridSize[1];
+        Controller.gridSize[0] = Math.round(imageWidth / parseInt(tile_Size.value));
+        Controller.gridSize[1] = Math.round(imageHeight / parseInt(tile_Size.value));
 
-    Controller.grid = new PF.Grid(numCols, numRows);
+        var numCols = Controller.gridSize[0],
+            numRows = Controller.gridSize[1];
+
+        Controller.grid = new PF.Grid(numCols, numRows);
 
 
-    View.init({
-        numCols: Math.round(imageWidth / parseInt(tile_Size.value)),
-        numRows: Math.round(imageHeight / parseInt(tile_Size.value))
-    });
+        View.init({
+            numCols: Math.round(imageWidth / parseInt(tile_Size.value)),
+            numRows: Math.round(imageHeight / parseInt(tile_Size.value))
+        });
 
-    View.generateGrid(function() {
-        Controller.setDefaultStartEndPos();
-        Controller.bindEvents();
-        //Controller.transition(); // transit to the next state (ready)
-    });
+        View.generateGrid(function() {
+            Controller.setDefaultStartEndPos();
+            Controller.bindEvents();
+            //Controller.transition(); // transit to the next state (ready)
+        });
 
-    var item = {};
-    item["imgHeight"] = $("svg").height();
-    item["imgWidth"] = $("svg").width();
-    item["tileSize"] = tile_Size.value;
-    scaleObj = item;
+        drawArea.setAttribute("style", "width:" + $("svg").width() + "px; height:" + $("svg").height() + "px; background-image: " + 'url("' + input.value + '");background-size: ' + $("svg").width() + 'px ' + $("svg").height() + 'px;');
 
-    drawArea.setAttribute("style", "width:" + $("svg").width() + "px; height:" + $("svg").height() + "px; background-image: " + 'url("' + input.value + '");background-size: ' + $("svg").width() + 'px ' + $("svg").height() + 'px;');
+        var item = {};
+        item["imgHeight"] = $("svg").height();
+        item["imgWidth"] = $("svg").width();
+        item["tileSize"] = tile_Size.value;
+        scaleObj = item;
+    };
+    downloadingImage.src = input.value;
+
 }
 
 addStore.onclick = function() {
@@ -95,20 +109,37 @@ addStore.onclick = function() {
     console.log(storeArray);
 }
 
-exportStore.onclick = function() {
-    var jsonStore = JSON.stringify(storeArray);
-
-    download("Store.json", jsonStore);
-}
-
 get_array.onclick = function() {
-    var gridArray = Controller.grid;
-    gridArray["storeInfo"] = storeArray;
-    gridArray["scaleInfo"] = scaleObj;
-    gridArray["imageInfo"] = input.value;
-    var jsonGrid = JSON.stringify(gridArray);
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
 
-    download("Grid.json", jsonGrid);
+    add_name.onclick = function() {
+        var grid_input_name = document.getElementById('grid_name');
+        var grid_input_description = document.getElementById('grid_description');
+        grid_name = grid_input_name.value;
+        grid_description = grid_input_description.value;
+        //check if the 
+        firebase.database().ref('/' + api_key + '/grids/' + grid_name).once('value').then(function(snapshot) {
+            grid = snapshot.val();
+            if (!grid) {
+                console.log(grid_name);
+                modal.style.display = "none";
+
+                var gridArray = Controller.grid;
+                gridArray["storeInfo"] = storeArray;
+                gridArray["scaleInfo"] = scaleObj;
+                gridArray["imageInfo"] = input.value;
+                var jsonGrid = JSON.stringify(gridArray);
+                writeGridData(grid_name, jsonGrid, grid_description);
+            } else {
+                alert("Error: That name already exits!");
+            }
+        });
+
+    }
+
+    //download("Grid.json", jsonGrid);
 }
 
 function getStore(gridX, gridY) {
